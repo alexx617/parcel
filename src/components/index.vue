5e02bf013e1a0cadca09c4a78b51c67842bc1f5f
alexx617 <290477385@qq.com>
2018-01-19 17:01:18 +0800

    first

---
+++ src/components/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="indexBox">
+        1231213132
+        <img src="../assets/images/test.png"/>
+    </div>
+</template>
+
+<script>
+const log = console.log;
+export default {
+    data () {
+        return {
+            
+        };
+    },
+    components:{},
+    created() {
+        
+    },
+    methods : {
+        
+    },
+}
+
+</script>
+
+<style scoped>
+.indexBox{
+    background: pink;
+    color: #fff;
+}
+</style>
